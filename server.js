0853af48361e28595d2dc53a8c8ec7ee893a92e7
kmural <kmdharan@gmail.com>
2019-02-10 01:12:55 -0500

    continue changes

--- server.js
+++ server.js
@@ -1,12 +1,60 @@
 const express = require("express");
 require("dotenv").config();
 const jwt = require("express-jwt");
-const jwks = require("jwks-rsa");
+const jwksRsa = require("jwks-rsa");
+const checkScope = require("express-jwt-authz");
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${
+      process.env.REACT_APP_AUTH0_DOMAIN
+    }/.well-known/jwks.json`
+  }),
+  audience: process.env.REACH_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+  algorithms: ["RS256"]
+});
+
 const app = express();
 
 app.get("/public", function(req, res) {
   res.json({
     message: "Hello from Public API!"
+  });
+});
+
+app.get("/private", checkJwt, function(req, res) {
+  res.json({
+    message: "Hello from Private API!"
+  });
+});
+
+const checkRole = role => {
+  return function(req, res, next) {
+    const assignedRoles = req.user["http://localhost:3000/roles"];
+    if (Array.isArray(assignedRoles) && assignedRoles.includes(role)) {
+      return next();
+    } else {
+      return res.status(401).send("Insufficient role");
+    }
+  };
+};
+
+app.get("/admin", checkJwt, checkRole("admin"), function(req, res) {
+  res.json({
+    message: "Hello from Admin API!"
+  });
+});
+
+app.get("/course", checkJwt, checkScope(["read:courses"]), function(req, res) {
+  res.json({
+    courses: [
+      { id: 1, title: "Building Apps with React and Redux" },
+      { id: 2, title: "Creating Reusable React Components" }
+    ]
   });
 });
 
--- src/App.js
+++ src/App.js
@@ -6,40 +6,77 @@ import Nav from "./Nav";
 import Auth from "./Auth/Auth";
 import Callback from "./Callback";
 import Public from "./Public";
+import Private from "./Private";
+import Courses from "./Courses";
+import AuthContext from "./AuthContext";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.auth = new Auth(this.props.history);
+    this.state = {
+      auth: new Auth(this.props.history),
+      tokenRenewalComplete: false
+    };
   }
 
+  componentDidMount = () => {
+    this.state.auth.renewToken(() =>
+      this.setState({ tokenRenewalComplete: true })
+    );
+  };
+
   render() {
+    const { auth } = this.state;
+    // if (!this.state.tokenRenewalComplete) return "Loading...";
+
     return (
-      <>
-        <Nav auth={this.auth} />
+      <AuthContext.Provider value={auth}>
+        <Nav auth={auth} />
         <div className="body">
           <Route
             path="/"
             exact
-            render={props => <Home auth={this.auth} {...props} />}
+            render={props => <Home auth={auth} {...props} />}
           />
           <Route
             path="/callback"
-            render={props => <Callback auth={this.auth} {...props} />}
+            render={props => <Callback auth={auth} {...props} />}
           />
           <Route
             path="/profile"
             render={props =>
-              this.auth.isAuthenticated() ? (
-                <Profile auth={this.auth} {...props} />
+              auth.isAuthenticated() ? (
+                <Profile auth={auth} {...props} />
               ) : (
                 <Redirect to="/" />
               )
             }
           />
           <Route path="/public" component={Public} />
+          <Route
+            path="/private"
+            exact
+            render={props =>
+              auth.isAuthenticated() ? (
+                <Private auth={auth} {...props} />
+              ) : (
+                auth.login
+              )
+            }
+          />
+          <Route
+            path="/course"
+            exact
+            render={props =>
+              auth.isAuthenticated() && auth.userHasScopes(["read:courses"]) ? (
+                <Courses auth={auth} {...props} />
+              ) : (
+                auth.login
+              )
+            }
+          />
         </div>
-      </>
+      </AuthContext.Provider>
     );
   }
 }
